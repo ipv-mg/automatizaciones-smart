5109b5665c571a5c387d512ba7c6987b4f4e817c
ipv-mg <ivan.principe@materiagris.pe>
2026-08-05 19:07:27 -0500

    feat: automatizacion de regularizacion de marcas
    registro, edicion y eliminacion de marcas para un solo día.

--- data/regularizacionData.ts
+++ data/regularizacionData.ts
@@ -3,15 +3,6 @@ dotenv.config();
 
 const defaultPassword = process.env.DEFAULT_QA_PASSWORD || '';
 
-export interface RegularizacionMarca {
-  correo: string;
-  password: string;
-  fecha: string;
-  hora: string[];
-  motivo: string;
-  texto: string;
-}
-
 export const MOTIVO = {
   SMART: "Problemas con Smart",
   BIOMETRICO: "Problemas con Biométrico",
@@ -21,13 +12,42 @@ export const MOTIVO = {
   OTROS: "Otros"
 } as const;
 
+export const TIPO = {
+  REGISTRAR: "REGISTRAR",
+  EDITAR: "EDITAR",
+  ELIMINAR: "ELIMINAR"
+} as const;
+
+export interface RegularizacionMarca {
+  correo: string;
+  password: string;
+  fecha: string;
+  /** estos datos representan los arreglos para manejar la regularización
+  @registrar → consume horaNueva
+  @editar → consume horaActual y horaNueva
+  @eliminar → consume horaActual
+  */
+  horaNueva: string[];
+  horaActual: string[]; 
+  horaEliminar: string[];
+  motivo: string;
+  texto: string;
+  /**
+  @tipo determina si el flujo a seguir es registrar, editar o eliminar
+  */
+  tipo: string;
+}
+
 export const regularizacion: RegularizacionMarca[] = [
   {
-    correo: "ivan.principe@materiagris.pe",
+    correo: "christian.romero@materiagris.pe",
     password: defaultPassword, 
-    fecha: "20 de julio de",
-    hora: ["09:00","13:00"],
+    fecha: "3 de agosto de 2026",
+    horaNueva: ["19:30"],
+    horaActual: ["19:00"],
+    horaEliminar: [""],
     motivo: MOTIVO.OTROS,
-    texto: "AUTOMATIZACION DE REGULARIZACION DE MARCA"
+    texto: "AUTOMATIZACION DE REGULARIZACION DE MARCA",
+    tipo: TIPO.EDITAR
   }
 ];
